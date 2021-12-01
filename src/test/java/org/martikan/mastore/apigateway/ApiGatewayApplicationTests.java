d03b6be693962c30fe768e47372c4d320da823fd
martikan <ric.martikan@gmail.com>
2021-12-02 00:57:02 +0100

    Add basic test environment.

--- src/test/java/org/martikan/mastore/apigateway/ApiGatewayApplicationTests.java
+++ src/test/java/org/martikan/mastore/apigateway/ApiGatewayApplicationTests.java
@@ -2,7 +2,9 @@ package org.martikan.mastore.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles(profiles = "test")
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
